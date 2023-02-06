cddb2d782cef64fc33876ecde40819dbcf260f6d
Kargin Maxim <karginmaxx@gmail.com>
2023-02-06 20:52:55 +0300

    modified Student.cs

--- ClassesLibrary/student.cs
+++ ClassesLibrary/student.cs
@@ -2,14 +2,14 @@
 
 namespace ClassesLibrary
 {
-    public class student
+    public class Student
     {
         private string name; // имя
         private string s_name; // фамилия
         private string p_name; // отчество
         private object group; // будет ссылка на группу
         private DateTime date; // дата рождения
-        student ( string name , string s_name, string p_name,object group,DateTime date) // конструктор со всей фигней
+        Student ( string name , string s_name, string p_name,object group,DateTime date) // конструктор со всей фигней
         {
             this.name = name;
             this.s_name = s_name;
