2bc87d10f0c0d3aaf1029e0041607b9fe9ef4d44
Kargin Maxim <karginmaxx@gmail.com>
2023-02-06 21:46:36 +0300

    Add Pair.cs

---
+++ ClassesLibrary/Pair.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    internal class Pair
+    {
+        private DateTime start;
+        private DateTime end;
+        private DateTime breakStart;
+        private DateTime breakEnd;
+        //ссылка на Смену
+        public Pair(DateTime start, DateTime end, DateTime breakStart, DateTime breakEnd)
+        {
+            this.start = start;
+            this.end = end;
+            this.breakStart = breakStart;
+            this.breakEnd = breakEnd;
+        }
+    }
+}
