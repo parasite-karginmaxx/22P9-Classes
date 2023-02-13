388d2556949652a9d3ef196af00be31ef131ab62
Kargin Maxim <88215412+parasite-karginmaxx@users.noreply.github.com>
2023-02-13 11:42:24 +0000

    Modified Pair.cs

--- ClassesLibrary/Pair.cs
+++ ClassesLibrary/Pair.cs
@@ -4,7 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClassesLibrary;
+namespace Kargin;
+
+#region Вариант 7
+//  https://docs.google.com/document/d/1r4OjKXg5_5yj6ibHB778UEF5Z1nbUqGdXjpQXbTGWiQ/edit
+#endregion
 
 internal class Pair
 {
@@ -13,12 +17,23 @@ internal class Pair
     private DateTime breakStart;
     private DateTime breakEnd;
     private Shift shift;
+
+    public Pair() : this(DateTime.Now()) {}
+    public Pair(DateTime start, DateTime end, Shift shift) : this(start, end, shift) {}
     public Pair(DateTime start, DateTime end, DateTime breakStart, DateTime breakEnd)
     {
         this.start = start;
         this.end = end;
         this.breakStart = breakStart;
         this.breakEnd = breakEnd;
-        shift = new Shift();
+        this.shift = new Shift();
+    }
+    public void Deconstructor(out DateTime dStart, out DateTime dEnd, out DateTime dBreakStart, out DateTime dBreakEnd, out Shift dShift)
+    {
+        dStart = start;
+        dEnd = end;
+        dBreakStart = breakStart;
+        dBreakEnd = breakEnd;
+        dShift = shift;
     }
 }
