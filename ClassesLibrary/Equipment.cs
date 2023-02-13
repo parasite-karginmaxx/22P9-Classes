444af9283b9198d60d6c05d9b4cab5626d6b592a
Валерия <the.only.cookie.lera@gmail.com>
2023-02-13 11:29:54 +0000

    Modified Equipment.cs

--- ClassesLibrary/Equipment.cs
+++ ClassesLibrary/Equipment.cs
@@ -1,16 +1,15 @@
-﻿namespace ClassesLibrary
-{
+﻿namespace ClassesLibrary;
+//  https://docs.google.com/document/d/1ynMOoROmPiCWqPt78fnwQoIJKsoAIgdxhzp1VSjatsE/edit
     public class Equipment
     {
         public string name;
         public decimal cost;
         public DateTime balance;
 
-        public Equipment(string name, DateTime balance = DateTime.Today)
+        public Equipment(string name, DateTime balance = DateTime.Today())
         {
             this.name = name;
             this.cost = cost;
             this.balance = balance;
         }
     }
-}
