6231a97d99c15cc741b30edc218b99c978dd3d28
Dokchansky <118961183+Dokchansky@users.noreply.github.com>
2023-02-22 00:48:36 +0300

    Внеурочка
    Я слишком глуп, чтобы сделать это идеально

---
+++ MySelfClass/MySelfClass/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary;
+#region Вариант 23
+// https://docs.google.com/document/d/1DUF8Z0-MH7np4AlSFXN5TYCDiut7WdsESiPi-BS8j-4/edit
+#endregion
+public class MySelfTraining
+{
+    private string name; // название
+    private object Employee; // ссылка на сотрудника
+    List<student> student; // список ссылок на студентов
+    MySelfTraining(string name, object student, object Employee) // конструктор
+    {
+        this.name = name;
+        this.student = student;
+        this.Employee = Employee;
+    }
+}
+
+
---
+++ MySelfClass/MySelfClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using ClassesLibrary;
+
+while (true)
+{
+    Console.WriteLine("Какой класс Вы хотите создать?");
+    switch (Console.ReadLine())
+    {
+        case "MySelftTraining":
+            Console.WriteLine(CreateMySelfTraining().name);
+            break;
+        default:
+            Console.WriteLine("Такого класса не существует");
+            break;
+    }
+    Console.WriteLine("Руководитель");
+    switch (Console.ReadLine())
+    {
+        default:
+            Console.WriteLine("Такой руководитель отсутствует");
+            break;
+    }
+}
