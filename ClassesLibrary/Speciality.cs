c0165aafe48aa685e1de373f3064e2321bbe3504
Nikitusic <118961619+Lucky133787@users.noreply.github.com>
2023-02-21 21:04:19 +0300

    Add files via upload

---
+++ ClassesLibrary/Speciality.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+namespace Classes
+{
+    public class Speciality
+    {
+        private string? title; //Название
+        public string? reduction; //Сокращение
+        Speciality(string? title, string? reduction)
+        {
+            this.title = title;
+            this.reduction = reduction;
+        }
+    }
+
+}
