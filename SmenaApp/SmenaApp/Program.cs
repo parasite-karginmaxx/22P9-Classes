3338e23fa67d49582f68de0460fc81f038935c83
Keecthup <118966408+Keecthup@users.noreply.github.com>
2023-02-21 20:59:58 +0300

    Название смены
    Сделал свой класс, но вряд ли это ктото читает

---
+++ SmenaApp/SmenaApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using ClassesLibrary;
+
+while (true)
+{
+    Console.WriteLine(CreateName().shfitname);
+    break;
+}
+    static Shift CreateName()
+{
+    Console.Write("Введите навзание смены: ");
+    return new Shift (Console.ReadLine() ?? "Нет названия");
+}
---
+++ SmenaApp/SmenaApp/Shift.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Shift  
+    {
+        private readonly string name;
+        public string shfitname { get => name; } 
+            
+        public Shift (string name)
+
+        { this.name = name; }
+
+    }
+}
