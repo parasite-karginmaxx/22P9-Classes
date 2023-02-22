d503235ecace34119cc87bd4912e43707a277e7d
Alendkr <124190831+Alendkr@users.noreply.github.com>
2023-02-22 12:03:23 +0300

    Speciality task

--- ClassesLibrary/Speciality.cs
+++ ClassesLibrary/Speciality.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+// Задание - " https://docs.google.com/document/d/1pDw7bbBJDmeqT8VVwp68BSOF_4yd9cNb9xpH_q2KW0E/edit "
+using System.Runtime.CompilerServices;
 namespace Classes
 {
     public class Speciality
