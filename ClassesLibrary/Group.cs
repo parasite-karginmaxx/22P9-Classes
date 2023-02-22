bca36075b0364492a6c72aa4ade723ba059e2be3
AlendK <alendk.real@gmail.com>
2023-02-22 11:55:08 +0300

    Upgrade

--- ClassesLibrary/Group.cs
+++ ClassesLibrary/Group.cs
@@ -1,4 +1,6 @@
-﻿namespace ClassesLibrary
+﻿using Classes;
+
+namespace ClassesLibrary
 /* https://docs.google.com/document/d/1qZEaADy71rJCOuNSTpyhh-f-NhSL2a6DCm88EgFO20o/edit */
 {
     public class Group
--- ClassesLibrary/Student.cs
+++ ClassesLibrary/Student.cs
@@ -1,15 +1,15 @@
-﻿using System.Runtime.CompilerServices;
-
+﻿//https://docs.google.com/document/d/1FrtPd2tz85cDVhZCYXqL0wrByPjNbd6kUbjXzOzfLPc/edit - задание
 namespace ClassesLibrary
 {
     public class Student
     {
-        private string name; // имя
-        private string s_name; // фамилия
-        private string p_name; // отчество
-        private object group; // будет ссылка на группу
+        private string? name; // имя
+        private string? s_name; // фамилия
+        private string? p_name; // отчество
+        private readonly Group group;
+        public Group Group { get => group; }
         private DateTime date; // дата рождения
-        Student ( string name , string s_name, string p_name,object group,DateTime date) // конструктор со всей фигней
+    Student ( string name , string s_name, string p_name,Group group,DateTime date) // конструктор со всей фигней
         {
             this.name = name;
             this.s_name = s_name;
