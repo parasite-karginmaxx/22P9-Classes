294e93b4a18f329d307c8e0a2599f688b09620fb
Егор <118961825+Jambo2@users.noreply.github.com>
2023-02-24 19:50:55 +0300

    Update Сorpus.cs

--- ClassesLibrary/Сorpus.cs
+++ ClassesLibrary/Сorpus.cs
@@ -1,14 +1,16 @@
-﻿
+//https://docs.google.com/document/d/13_TXjBoKn0TzKMcWP9A6jX2vyQEgcxBc4vY1CfJn9ps/edit
 
 namespace ClassesLibrary
 {
 	public class corpus
 	{
-		public string Title;
-		public string Address;
-		public employee commandant;
-		public Organization organization;
-        public corpus(string Title,string Address,employee commandant, Organization organization)
+		public string? Title;
+		public string? Address;
+		public readonly employee commandant;
+		public employee commandant {get => commandant;}
+		public readonly  Organization organization;
+		public Organization organization{get => organization;}
+        public corpus(string? Title,string? Address,employee commandant, Organization organization)
 		{
 			this.Title = Title;
 			this.Address = Address;
