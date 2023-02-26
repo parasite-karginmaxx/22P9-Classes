77f62394c9af2642821459ecb4bfee93c789ce3f
Mu3aHTp0n <107411576+MightyBeer@users.noreply.github.com>
2023-02-26 20:26:45 +0300

    Add files via upload

---
+++ ClassesLibrary/TypeOccupation.cs
@@ -0,0 +1,36 @@
+﻿//https://docs.google.com/document/d/1mwSWGxZfPEK4Ysyh9TJ4A_Ym1bew2ttNGWbMpK9y_zc/edit
+
+using ClassesLibrary;
+
+while (true)
+{
+    Console.WriteLine("Введите имя класса:");
+    switch (Console.ReadLine())
+    {
+        case "Никита бездарь(шучу)":
+            Console.WriteLine(CreateTypeOccupation().Name);
+            break;
+        default:
+            Console.WriteLine("Введите другое название");
+            break;
+    }
+}
+
+static TypeOccupation CreateTypeOccupation()
+{
+    Console.Write("Введите название занятия: ");
+    return new TypeOccupation(Console.ReadLine() ?? "Нет названия");
+}
+
+namespace ClassesLibrary
+{
+    public class TypeOccupation
+    {
+        private readonly string name;
+        public string Name { get => name; }
+        public TypeOccupation(string name)
+        {
+            this.name = name;
+        }
+    }
+}
